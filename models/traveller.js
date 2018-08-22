e1d78aa07306682a7215dd6297ef53d3da42dfce
janeduncan <janeduncan@live.co.uk>
2018-08-22 20:46:05 +0100

    final commit

--- models/traveller.js
+++ models/traveller.js
@@ -3,32 +3,46 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map ((journey) => {
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getModesOfTransport = function () {
-
+  return this.journeys.map ((mode) => {
+    return mode.transport;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0);
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-  
+  return Array.from(new Set(this.getModesOfTransport()))
 };
 
-
 module.exports = Traveller;
+
+// array = ['train', 'aeroplane', 'car', 'ferry', 'car']
+// return array.filter((x, i, a) => a.indexOf(x) == i)
